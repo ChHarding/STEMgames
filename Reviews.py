9106cd080039601e03b0dfebedb6a4c47ac8d5f3
Ezequiel Aleman <ealeman@iastate.edu>
2021-06-17 17:18:01 -0500

    Limited number of reviews to last 365 days - English only

--- Reviews.py
+++ Reviews.py
@@ -1,4 +1,10 @@
 #In this file I will scrape the data from STEAM reviews using the API. 
 #Start by building list of games. 
 import steamreviews
-steamreviews.download_reviews_for_app_id_batch()
+
+request_params = dict()
+request_params['filter'] = 'updated'
+request_params['day_range'] = '365'
+request_params['language'] = 'english'
+steamreviews.download_reviews_for_app_id_batch(chosen_request_params=request_params)
+                                                                
